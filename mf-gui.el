be9bcdffb18f3ae21ba117634ee55cf0c8b87dcd
Max Fujimoto <max.fujimoto@rogers.com>
2021-09-01 11:25:19 -0400

    Removed lame-ass normie Toolbar and Menu GUI

--- mf-gui.el
+++ mf-gui.el
@@ -1,5 +1,11 @@
-					;-------   Brin   -------\
+					;-------   Theme   -------\
 
 (use-package sublime-themes
    :config
    (load-theme 'brin t))
+
+					;-------   Gui-Menus   -------\
+(menu-bar-mode -1) 
+(tool-bar-mode -1) 
+
+(global-set-key (kbd "<f5>") 'menu-bar-mode)
