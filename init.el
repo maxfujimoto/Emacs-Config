0cd139ed3bcbde9cb6ddd9d7e3822e8039da698c
Max Fujimoto <max.fujimoto@rogers.com>
2021-09-17 23:16:24 -0400

    Fixed use-package install and require order for first boot

--- init.el
+++ init.el
@@ -12,6 +12,25 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
+
+					;-------   Custom   -------\
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package sublime-themes ebib org-bullets))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
+
+
+					;------- use-package -------\
+
+
 (eval-when-compile
   (require 'use-package))
 
@@ -21,17 +40,3 @@
 ;(load "~/.emacs.d/mf-keys.el")
 (load "~/.emacs.d/mf-org.el")
 (load "~/.emacs.d/mf-templates.el")
-
-					;-------   Custom   -------\
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (sublime-themes ebib org-bullets))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
