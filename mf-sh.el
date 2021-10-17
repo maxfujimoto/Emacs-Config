35b189516c3df338ae9b6724569b88d618bce7d4
Max <max.fujimoto@rogers.com>
2021-10-16 21:56:16 -0400

    Xrdb aftersave hook.

--- mf-sh.el
+++ mf-sh.el
@@ -9,31 +9,32 @@
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'mf/org-babel-tangle-config)))
 
 
-;;  (defun mf/xrdb-xresources ()
-;;    (when (string-equal (file-name-directory (buffer-file-name)
-;; 					    (expand-file-name ~/.Xresources))
-;; 		   (let ((shell-command
+(defun mf/xrdb-xresources ()
+  (when (string-equal (buffer-file-name)
+		      (expand-file-name "~/.dotfiles/.Xresources"))
+    (and (shell-command "xrdb ~/.Xresources")
+	 (message "xrdb updated"))))
 
-;; ;; (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'mf/xrdb-xresources)))
+
+(add-hook 'conf-mode-hook (lambda () (add-hook 'after-save-hook #'mf/xrdb-xresources)))
 
 
-;;     ;(defun xrdb-xresources ()
-;;  "Running xrdb ~/.Xresources"
-;;  (when (eq major-mode 'conf-mode)
-;;    (shell-command-to-string (format "xrdb ~/.Xresources %s" buffer-file-name))))
+;; (defun mf/dropbox ()
+;;   (while (string-equal (file-name-directory)
+;; 		       (expand-file-name "~/Dropbox"))
+;;     (shell-command "dropbox")))
 
-;;(add-hook 'after-save-hook #'xrdb-xresources)
+;; (add-hook ??)
 
-;;(defvar xresrouces
-;; '(("~/.Xresources" . "xrdb ~/.Xresrouces"))
-;; )
+     ;;     ;(defun xrdb-xresources ()
+     ;;  "Running xrdb ~/.Xresources"
+     ;;  (when (eq major-mode 'conf-mode)
+     ;;    (shell-command-to-string (format "xrdb ~/.Xresources %s" buffer-file-name))))
 
-;;(defun xrdb-xresources ()
-;;  "xrdb ~/.Xresource"
-;;  (let* ((match (assoc (buffer-file-name) xresrouces)))
-;;    (when match
-;;      (shell-command (cdr match)))))
+     ;;(defun xrdb-xresources ()
+     ;;  "xrdb ~/.Xresource"
+     ;;  (let* ((match (assoc (buffer-file-name) xresrouces)))
+     ;;    (when match
+     ;;      (shell-command (cdr match)))))
 
-	;(add-hook 'after-save-hook 'xrdb-xresources)
-
-(setq gc-cons-threshold (* 2 1000 1000))
+     (setq gc-cons-threshold (* 2 1000 1000))
