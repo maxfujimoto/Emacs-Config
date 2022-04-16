0d4ccaeef152dd79ac581b2ef1b6eda8626a51d3
Max <max.fujimoto@rogers.com>
2022-04-15 23:52:00 -0400

    Formating and mode-line visual bell

--- mf-gui.el
+++ mf-gui.el
@@ -8,6 +8,14 @@
 (set-fringe-mode 10)                    ; padding
 (tooltip-mode -1)
 ;;(setq visible-bell t)
+(setq ring-bell-function
+ (lambda ()
+   (let ((orig-fg (face-foreground 'mode-line)))
+     (set-face-foreground 'mode-line "#F2804F")
+     (run-with-idle-timer 0.1 nil
+			  (lambda (fg) (set-face-foreground 'mode-line fg))
+			  orig-fg))))
+
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
